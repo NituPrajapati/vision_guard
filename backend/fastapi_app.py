84d0a53241e958bcf901999a0d735326086a121a
NituPrajapati <prajapatinitu3425@gmail.com>
2025-11-02 11:47:16 +0530

    Google Auth Added

--- backend/fastapi_app.py
+++ backend/fastapi_app.py
@@ -68,7 +68,7 @@ oauth.register(
     client_id=Config.GOOGLE_CLIENT_ID,
     client_secret=Config.GOOGLE_CLIENT_SECRET,
     server_metadata_url="https://accounts.google.com/.well-known/openid-configuration",
-    client_kwargs={"scope": "openid email profile"},
+    client_kwargs={"scope": "openid email profile", "prompt": "select_account"},
 )
 
 def get_user_info_from_cookie(request: Request) -> Optional[dict]:
